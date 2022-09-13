20e6f940b66263aeeae2aa449f379b8eea2b0795
John <jn183116@gmail.com>
2022-09-13 12:41:36 -0500

    Fix readme formatting, copyright, header logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div>
             <p>MILLSAP INDEPENDENT SCHOOL DISTRICT</p>
-            <p>Prepared by <span><a target='blank' href='https://pbk.com/?gclid=Cj0KCQjwl92XBhC7ARIsAHLl9am0gppjdLyl7z6mXIUKOWmMU881Bhrcbr_G8ybKNRiKXy0QN4wFx94aAmH-EALw_wcB'>PBK Architects</a></span> copywrite 2022</p>
+            <p>Prepared by <span><a target='blank' href='https://pbk.com/?gclid=Cj0KCQjwl92XBhC7ARIsAHLl9am0gppjdLyl7z6mXIUKOWmMU881Bhrcbr_G8ybKNRiKXy0QN4wFx94aAmH-EALw_wcB'>PBK Architects</a></span> copyright 2022</p>
         </div>
         
     )
